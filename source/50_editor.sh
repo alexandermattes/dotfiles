92439593a10c8e30ff78e6a5a334ebc1c94c5d3c
Ben Alman <EMAIL>
2015-10-30 17:05:58 +0100

    Comment out not needed code

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -8,6 +8,6 @@ else
 fi
 
 export VISUAL="$EDITOR"
-alias q="$EDITOR"
-alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
-alias qs="q $DOTFILES"
+# alias q="$EDITOR"
+# alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
+# alias qs="q $DOTFILES"
