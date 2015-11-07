4749764db51f13646f59926fc86f2c242c2ea100
Alexander Mattes <alexm128@googlemail.com>
2015-11-07 13:24:15 +0100

    Add aliases for easier apt-get usage

--- source/50_ubuntu.sh
+++ source/50_ubuntu.sh
@@ -2,10 +2,10 @@
 is_ubuntu || return 1
 
 # # Package management
-# alias update="sudo apt-get -qq update && sudo apt-get upgrade"
-# alias install="sudo apt-get install"
-# alias remove="sudo apt-get remove"
-# alias search="apt-cache search"
+alias update="sudo apt-get -qq update && sudo apt-get upgrade"
+alias install="sudo apt-get install"
+alias remove="sudo apt-get remove"
+alias search="apt-cache search"
 
 # Make 'less' more.
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
