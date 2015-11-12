7584ff8f734a81a52f4d587977b8b7595a6c3ef1
Alexander Mattes <alexm128@googlemail.com>
2015-11-12 10:45:01 +0100

    Add ghc-pkg-reset command

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -33,3 +33,16 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
+
+# ghc-pkg-reset
+# Removes all installed GHC/cabal packages, but not binaries, docs, etc.
+# Use this to get out of dependency hell and start over, at the cost of some rebuilding time.
+function ghc-pkg-reset() {
+    read -p 'erasing all your user ghc and cabal packages - are you sure (y/n) ? ' ans
+    test x$ans == xy && ( \
+        echo 'erasing directories under ~/.ghc'; rm -rf `find ~/.ghc -maxdepth 1 -type d`; \
+        echo 'erasing ~/.cabal/lib'; rm -rf ~/.cabal/lib; \
+        # echo 'erasing ~/.cabal/packages'; rm -rf ~/.cabal/packages; \
+        # echo 'erasing ~/.cabal/share'; rm -rf ~/.cabal/share; \
+        )
+}
