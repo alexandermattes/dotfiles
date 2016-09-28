5362e05c53b6508b25d2566023a7647e911eb9f5
Alexander Mattes <alexm128@gmail.com>
2016-09-28 11:06:44 +0200

    Change bash language to english
    -Language for time, addresses etc. remains german

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -1,3 +1,21 @@
+# Change language in bash to english
+export LANG=en_GB.UTF-8
+export LANGUAGE=
+export LC_CTYPE="de_DE.UTF-8"
+export LC_NUMERIC="de_DE.UTF-8"
+export LC_TIME="de_DE.UTF-8"
+export LC_COLLATE="de_DE.UTF-8"
+export LC_MONETARY="de_DE.UTF-8"
+export LC_MESSAGES="en_GB.UTF-8"
+export LC_PAPER="de_DE.UTF-8"
+export LC_NAME="de_DE.UTF-8"
+export LC_ADDRESS="de_DE.UTF-8"
+export LC_TELEPHONE="de_DE.UTF-8"
+export LC_MEASUREMENT="de_DE.UTF-8"
+export LC_IDENTIFICATION="de_DE.UTF-8"
+export LC_ALL=
+
+
 # Case-insensitive globbing (used in pathname expansion)
 shopt -s nocaseglob
 
